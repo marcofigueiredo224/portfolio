969277388f7ae5c8e1d75e1748307cff06514125
Marco Figueiredo <marco.figueiredo@blueservice.com.br>
2023-10-03 16:10:13 -0300

    Carrossel.

---
+++ javas.js
@@ -0,0 +1,30 @@
+// scripts.js
+
+let isDragging = false;
+let startX;
+let scrollLeft;
+
+const carousel = document.getElementById('carousel');
+
+carousel.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startX = e.pageX - carousel.offsetLeft;
+    scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+    isDragging = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - carousel.offsetLeft;
+    const walk = (x - startX) * 1; // Ajuste a sensibilidade de arrastar aqui
+
+    carousel.scrollLeft = scrollLeft - walk;
+});
